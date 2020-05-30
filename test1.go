b18aff3f594fa153efe5c7f530ec6788a4d2df22
guyan0319 <guyan0319@163.com>
2020-05-30 23:56:56 +0800

    test

--- test1.go
+++ test1.go
@@ -16,15 +16,21 @@ type CustomClaims struct {
 func main() {
 	//生成token
 	maxAge:=60*60*24
-
 	// Create the Claims
-	claims := &jwt.StandardClaims{
-		ExpiresAt: time.Now().Add(time.Duration(maxAge)*time.Second).Unix(), // 过期时间，必须设置,
-		Issuer:    "jerry",// 非必须，也可以填充用户名，
+	//claims := &jwt.StandardClaims{
+	//	//	ExpiresAt: time.Now().Add(time.Duration(maxAge)*time.Second).Unix(), // 过期时间，必须设置,
+	//	//	Issuer:    "jerry",// 非必须，也可以填充用户名，
+	//	//}
+
+	//或者用下面自定义claim
+	claims := jwt.MapClaims{
+		"id":       11,
+		"name":       "jerry",
+		"exp": time.Now().Add(time.Duration(maxAge)*time.Second).Unix(), // 过期时间，必须设置,
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(SECRETKEY)
+	tokenString, err := token.SignedString([]byte(SECRETKEY))
 	if err!=nil {
 		fmt.Println(err)
 	}
@@ -39,7 +45,7 @@ func main() {
 }
 
 //解析token
-func ParseToken(tokenString string)(*CustomClaims,error)  {
+func ParseToken(tokenString string)(jwt.MapClaims,error)  {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -49,7 +55,7 @@ func ParseToken(tokenString string)(*CustomClaims,error)  {
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 		return []byte(SECRETKEY), nil
 	})
-	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		return claims,nil
 	} else {
 		return nil,err
